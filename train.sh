48b4e689834e7dfd9ba3f26c33a36368fb1bb100
yun-liu <vagrantlyun@gmail.com>
2021-09-27 16:38:23 +0200

    add train.sh

---
+++ train.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rsync -aq ./ ${TMPDIR}
+
+cd $TMPDIR
+
+mkdir data
+
+tar -I pigz -xf /cluster/work/cvl/liuyun/Edges/HED-BSDS.tar.gz -C ${TMPDIR}/data
+
+python train.py --save-dir /cluster/home/liuyun/data/Models/RCF
